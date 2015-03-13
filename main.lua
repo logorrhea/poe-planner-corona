b2bae94cada196d04952af8a07f65ea1035fa8cb
Tyler Funk <tfunk@fastmail.fm>
2015-03-13 01:17:35 -0500

    Camera is traggable and nodes are selectable.

--- main.lua
+++ main.lua
@@ -15,34 +15,67 @@ local perspective = require("perspective")
 require("jsonDecoder")
 
 local camera = perspective.createView()
+local width = display.pixelWidth
+local height = display.pixelHeight
+local tracker = display.newRect(height/2, width/2, height, width)
+tracker:setFillColor(0.5, 0.25)
+camera:setFocus(tracker)
 
-local function drawNode(node)
-   x = node.location.x
-   y = node.location.y
-   tier = node.tier
-   radius = 20 + 10*node.tier
-   camera.add(display.newCircle(x, y, radius))
-end
+off = { 1, 0, 0}
+on  = { 0, 1, 0 }
+
+camera:track()
 
 -- Read JSON data from file
 local path = system.pathForFile("combined_node_data.json", system.ResourceDirectory)
 local file = io.open(path, "r")
 local nodes = json.decode(file:read("*a"))
 
+function drawNode(node)
+   x = node.location.x
+   y = node.location.y
+   tier = node.tier
+   radius = 20 + 10*node.tier
+   local circ = display.newCircle(x, y, radius)
+   circ.fill = off
+   circ.active = false
+   function circ:tap(e)
+	  if self.active then
+		 self.fill = off
+	  else
+		 self.active = true
+		 self.fill = on
+	  end
+	  return true
+   end
+   circ:addEventListener("tap", circ)
+   camera:add(circ, 1)
+end
+
+function dragListener(e)
+   if(e.phase == "began") then
+	  lastX = e.xStart
+	  lastY = e.yStart
+	  print(e.xStart)
+   elseif(e.phase == "moved") then
+	  local moveX = e.x - lastX
+	  local moveY = e.y - lastY
+	  tracker.x, tracker.y = (tracker.x + moveX), (tracker.y + moveY)
+	  lastX, lastY = e.x, e.y
+	  print(e.xStart)
+   elseif(e.phase == "ended" or e.phase == "cancelled") then
+   end
+
+   return false
+end
+
 for _, node in ipairs(nodes) do
    drawNode(node)
 end
 
-local function dragListener(event)
-   if(event.phase == "began") then
-	  print("began")
-   elseif(event.phase == "moved") then
-	  print("moved")
-   elseif(event.phase == "ended" or event.phase == "cancelled") then
-	  print("ended")
-   end
+local lastX = nil
+local lastY = nil
 
-   return true
-end
 
 Runtime:addEventListener("touch", dragListener)
+
